6047ef9807a2208aa80d5ce009c0cc0c3be000a6
Edoardo.Desiderio <edoardo.desiderio01@gmail.com>
2023-02-25 18:31:08 +0100

    passing primaryStage as arg to view

--- src/main/java/it/unibo/game/ArkanoidApp.java
+++ src/main/java/it/unibo/game/ArkanoidApp.java
@@ -1,43 +1,30 @@
 package it.unibo.game;
 
+import java.awt.Dimension;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.stage.Stage;
 
-public final class ArkanoidApp extends Application{
-    
+public final class ArkanoidApp extends Application {
+
     private Model model;
     private View view;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         model = new Model();
-        view = new View();
-
+        view = new View(primaryStage);
 
         // Wire up the increment button
-        view.getIncrementButton().setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                model.incrementValue();
-                view.setValueLabel(model.getValue());
-            }
+        view.getIncrementButton().setOnAction(EventHandler -> {
+            model.incrementValue();
+            view.setValueLabel(model.getValue());
         });
 
         // Wire up the decrement button
-        view.getDecrementButton().setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
+        view.getDecrementButton().setOnAction(EventHandler->{
                 model.decrementValue();
                 view.setValueLabel(model.getValue());
-            }
         });
-    }
-
-    @Override
-    public void stop() throws Exception {
-       
-        super.stop();
     }
 }
