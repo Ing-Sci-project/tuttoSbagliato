d3dd0c708a95d241914d6ab3066a1735ce74af9e
Edoardo.Desiderio <edoardo.desiderio01@gmail.com>
2023-02-25 13:46:50 +0100

    redefine the structure of the project

--- src/main/java/it/unibo/game/ArkanoidApp.java
+++ src/main/java/it/unibo/game/ArkanoidApp.java
@@ -1,17 +1,43 @@
 package it.unibo.game;
 
-import java.io.FileNotFoundException;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
 
-public final class ArkanoidApp {
+public final class ArkanoidApp extends Application{
     
-    ArkanoidApp(){}
-    /**
-    * @param args
-    *            ignored
-    * @throws FileNotFoundException 
-    */
-   public static void main(final String... args) throws FileNotFoundException {
-       Application.launch(Controller.class);
-   }
+    private Model model;
+    private View view;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        model = new Model();
+        view = new View();
+
+
+        // Wire up the increment button
+        view.getIncrementButton().setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                model.incrementValue();
+                view.setValueLabel(model.getValue());
+            }
+        });
+
+        // Wire up the decrement button
+        view.getDecrementButton().setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                model.decrementValue();
+                view.setValueLabel(model.getValue());
+            }
+        });
+    }
+
+    @Override
+    public void stop() throws Exception {
+       
+        super.stop();
+    }
 }
--- src/main/java/it/unibo/game/View.java
+++ src/main/java/it/unibo/game/View.java
@@ -1,8 +1,11 @@
 package it.unibo.game;
 
 import java.awt.Dimension;
+import java.io.IOException;
 
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,9 +18,9 @@ public class View {
     private Button incrementButton;
     private Button decrementButton;
 
-    public View() {
+    public View() throws IOException {
         
-            valueLabel = new Label("0");
+        /*valueLabel = new Label("0");
 
             incrementButton = new Button("Increment");
             decrementButton = new Button("Decrement");
@@ -28,13 +31,14 @@ public class View {
 
             VBox root = new VBox(30);
             root.setPadding(new Insets(10));
-            root.getChildren().addAll(valueLabel, buttonBox);
+            root.getChildren().addAll(valueLabel, buttonBox); */
          
         // Set up the stage
         /*creo una finestra in base la dimensione dello schermo in cui viene 
         * lanciata l'applicazione
         Parent root = FXMLLoader.load(getClass().getResource(""));
         */
+        final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/counter.fxml"));
         
         try {
             Stage stage = new Stage();
