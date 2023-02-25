412ddd7ef21777299852ce4b60993c9078295c55
Edoardo.Desiderio <edoardo.desiderio01@gmail.com>
2023-02-25 18:31:42 +0100

    the root not working

--- src/main/java/it/unibo/game/View.java
+++ src/main/java/it/unibo/game/View.java
@@ -2,54 +2,33 @@ package it.unibo.game;
 
 import java.awt.Dimension;
 import java.io.IOException;
+import java.net.URL;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-public class View {
+public class View  {
+    @FXML
     private Label valueLabel;
-    private Button incrementButton;
-    private Button decrementButton;
+    @FXML
+    private Button decBtn;
+    @FXML
+    private Button incBtn;
+    final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/Counter.fxml"));
 
-    public View() throws IOException {
+    public View(Stage primaryStage) throws IOException {
         
-        /*valueLabel = new Label("0");
+        Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        primaryStage.setMinWidth(screenSize.getWidth() / 3);
+        primaryStage.setMinHeight(screenSize.getHeight() / 2);
+        primaryStage.setTitle("Arkanoid");
+        primaryStage.setScene(new Scene(root, primaryStage.getMinWidth(), primaryStage.getMinHeight()));
+        primaryStage.show();
 
-            incrementButton = new Button("Increment");
-            decrementButton = new Button("Decrement");
-
-            HBox buttonBox = new HBox(10);
-            buttonBox.getChildren().addAll(incrementButton, decrementButton);
-
-
-            VBox root = new VBox(30);
-            root.setPadding(new Insets(10));
-            root.getChildren().addAll(valueLabel, buttonBox); */
-         
-        // Set up the stage
-        /*creo una finestra in base la dimensione dello schermo in cui viene 
-        * lanciata l'applicazione
-        Parent root = FXMLLoader.load(getClass().getResource(""));
-        */
-        final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/counter.fxml"));
-        
-        try {
-            Stage stage = new Stage();
-            Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-            stage.setMinWidth(screenSize.getWidth() / 3);
-            stage.setMinHeight(screenSize.getHeight() / 2);
-            stage.setScene(new Scene(root, stage.getMinWidth(), stage.getMinHeight()));
-			stage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
     }
 
     public void setValueLabel(int value) {
@@ -57,11 +36,10 @@ public class View {
     }
 
     public Button getIncrementButton() {
-        return incrementButton;
+        return incBtn;
     }
 
     public Button getDecrementButton() {
-        return decrementButton;
+        return decBtn;
     }
 }
-
